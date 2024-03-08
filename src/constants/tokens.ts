025f849a7ccb1f24219df97c5d21526dafc41627
knstntn <konstantin@quadrate.org>
2024-03-08 12:20:15 +0500

    feat: add new tokens in TokenInfo
    Co-authored-by: Konstantin <konstantin@quadrate.org>

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -3,7 +3,9 @@ import ATOMLogo from "../assets/svg/AtomLogo.webp";
 import QubeLogo from "../assets/svg/Qube Logo Circle.webp";
 import WBTCLogo from "../assets/svg/WBTCLogo.webp"
 import USDCLogo from "../assets/svg/USDC Logo.webp"
-
+import WETHLogo from "../assets/svg/WETHLogo.webp"
+import USDTLogo from "../assets/svg/USDT Logo.webp"
+import SOLLogo from "../assets/svg/SOL Logo.webp"
 
 interface TokenInfo {
         Denom: string
@@ -40,6 +42,27 @@ export const TOKEN_INFO: TokenInfo[] = [
                 Base: "USDC",
                 Network: "USDC",
                 Logo: USDCLogo,
+                Decimals: 6
+        },
+        {
+                Denom: "factory/qube1t2ydw7r4asmk74ymuvykjshdzun8dxye0az5wz/uweth",
+                Base: "ETH",
+                Network: "Ethereum",
+                Logo: WETHLogo,
+                Decimals: 6
+        },
+        {
+                Denom: "factory/qube1t2ydw7r4asmk74ymuvykjshdzun8dxye0az5wz/uusdt",
+                Base: "USDT",
+                Network: "USDT",
+                Logo: USDTLogo,
+                Decimals: 6
+        },
+        {
+                Denom: "factory/qube1t2ydw7r4asmk74ymuvykjshdzun8dxye0az5wz/uusol",
+                Base: "SOL",
+                Network: "Solana Network",
+                Logo: SOLLogo,
                 Decimals: 6
         },
         {
