304682730d095b7f742f02b53e1a716dda08c6b0
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-03-11 00:16:14 +0500

    fix:

--- src/components/Page/Liquidation/Liquidation.tsx
+++ src/components/Page/Liquidation/Liquidation.tsx
@@ -14,19 +14,16 @@ const Block = styled.div <{backgroundColor: string}>`
     height: calc(100vh - 65px);
     background: ${props => props.backgroundColor};
     margin-top: -5px;
+    display: flex;
+    align-items: center;
 `
 
 const TestText = styled.h1`
     font-size: 30px;
-    margin-top: 14em;
-    margin-left: -30px;
     background: linear-gradient(to right, #77bff9, #2d96ff);
     background-clip: border-box;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
-    @media (max-width: 500px) {
-        margin-top: 11em;
-    }
 `
 
 export const Liquidation = () => {
